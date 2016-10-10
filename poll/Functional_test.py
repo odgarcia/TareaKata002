2561ae6198682372abe5cfd656a25289fbb0d8cf
odgarcia <od.garcia@uniandes.edu.co>
2016-10-09 19:50:42 -0500

    Paso 1- Test para probar el titulo de la pagina inicial

---
+++ poll/Functional_test.py
@@ -0,0 +1,16 @@
+__author__ = "oscar"
+from unittest import TestCase
+from selenium import webdriver
+
+class FuntionalTest(TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_title(self):
+        self.browser.get('http://localhost:8000')
+        self.assertIn('Busco Ayuda', self.browser.title)
+
