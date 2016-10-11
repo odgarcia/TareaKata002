2b46415708a8ceef3f3f3a2c778f52c28cdd6852
odgarcia <od.garcia@uniandes.edu.co>
2016-10-11 18:39:08 -0500

    Paso 5- Test para probar la edicion de datos de un independiente (Rojo)

--- poll/Functional_test.py
+++ poll/Functional_test.py
@@ -63,6 +63,7 @@ class FuntionalTest(TestCase):
     #     h2 = self.browser.find_element_by_xpath('//h2[text()="Pedro Perez"]')
     #     self.assertIn('Pedro Perez', h2.text)
 
+
     def test_ingreso(self):
         self.browser.get('http://localhost:8000')
         link = self.browser.find_element_by_id('id_login')
@@ -83,6 +84,64 @@ class FuntionalTest(TestCase):
         mensajeFlotante = self.browser.find_element_by_class_name('float-message')
         textMensaje = mensajeFlotante.text
         self.assertTrue(textMensaje.index('SUCCESS: Bienvenido al sistema pedro456'))
+
+
+    def test_editar(self):
+        self.browser.get('http://localhost:8000')
+        self.browser.get('http://localhost:8000')
+        link = self.browser.find_element_by_id('id_login')
+        link.click()
+
+        self.browser.implicitly_wait(3)
+
+        textUserName = self.browser.find_element_by_id("id_username")
+        textUserName.send_keys('pedro456')
+
+        textPassword = self.browser.find_element_by_id("id_password")
+        textPassword.send_keys('clave123')
+
+        botonIngreso = self.browser.find_element_by_id('id_ingreso')
+        botonIngreso.click()
+
+        self.browser.implicitly_wait(2)
+        linkEditar = self.browser.find_element_by_id('id_editar')
+        linkEditar.click()
+
+        self.browser.implicitly_wait(1)
+
+        self.browser.implicitly_wait(3)
+
+        nombre = self.browser.find_element_by_id('id_nombre')
+        nombre.send_keys('Carlos')
+
+        apellidos = self.browser.find_element_by_id('id_apellidos')
+        apellidos.send_keys('Santana')
+
+        experiencia = self.browser.find_element_by_id('id_aniosExperiencia')
+        experiencia.send_keys('3')
+
+        self.browser.find_element_by_xpath("//select[@id='id_tiposDeServicio']/option[text()='Desarrollador Web']").click()
+        telefono = self.browser.find_element_by_id('id_telefono')
+        telefono.send_keys('3013648285')
+
+        correo = self.browser.find_element_by_id('id_correo')
+        correo.send_keys('od.garcia@uniandes.edu.co')
+
+        imagen = self.browser.find_element_by_id('id_imagen')
+        imagen.send_keys('C:\Users\oscar\Downloads\yo.jpg')
+
+        botonGrabar = self.browser.find_element_by_id('id_grabar')
+        botonGrabar.click()
+
+
+        self.browser.implicitly_wait(3)
+        span=self.browser.find_element_by_xpath( '//span[text()="Carlos Santana"]')
+
+        self.assertIn('Carlos Santana', span.text)
+        self.assertNotIn('Pedro Perez', span.text)
+
+
+
 
 
 
